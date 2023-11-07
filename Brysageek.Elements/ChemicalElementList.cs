6e3dd1a4cb007f608dc524117fdba1063b2e18cc
Brian Locke <brysageek@gmail.com>
2023-11-06 22:33:33 -0800

    End of night commit

--- Brysageek.Elements/ChemicalElementList.cs
+++ Brysageek.Elements/ChemicalElementList.cs
@@ -27,6 +27,70 @@
             Add(new ChemicalElement(14, 2, 6,
                 12.0107f, "Carbon", "C", 1086.5f,
                 2.55f, MetalClassificationTypes.NonMetal));
+
+            Add(new ChemicalElement(15, 2, 7,
+                14.0067f, "Nitrogen", "N", 1402.3f,
+                3.04f, MetalClassificationTypes.NonMetal));
+
+            Add(new ChemicalElement(16, 2, 8,
+                15.9994f, "Oxygen", "O", 1313.9f,
+                3.44f, MetalClassificationTypes.NonMetal));
+
+            Add(new ChemicalElement(17, 2, 9,
+                18.998403f, "Fluorine", "F", 1681.0f,
+                3.98f, MetalClassificationTypes.Halogen));
+
+            Add(new ChemicalElement(18, 2, 10,
+                20.1797f, "Neon", "Ne", 2080.7f,
+                0.0f, MetalClassificationTypes.NobleGas));
+
+            Add(new ChemicalElement(1, 3, 11,
+                22.98976f, "Sodium", "Na", 495.8f,
+                0.93f, MetalClassificationTypes.AlkaliMetal));
+
+            Add(new ChemicalElement(2, 3, 12,
+                24.3050f, "Magnesium", "Mg", 737.7f,
+                1.31f, MetalClassificationTypes.AlkalineMetal));
+
+            Add(new ChemicalElement(13, 3, 13,
+                26.98153f, "Aluminum", "Al", 577.5f,
+                1.61f, MetalClassificationTypes.OtherMetal));
+
+            Add(new ChemicalElement(14, 3, 14,
+                28.0855f, "Silicon", "Si", 786.5f,
+                1.90f, MetalClassificationTypes.Metalloid));
+
+            Add(new ChemicalElement(15, 3, 15,
+                30.97696f, "Phosphorus", "P", 1011.8f,
+                2.19f, MetalClassificationTypes.NonMetal));
+
+            Add(new ChemicalElement(16, 3, 16,
+                32.065f, "Sulfur", "S", 999.6f,
+                2.58f, MetalClassificationTypes.NonMetal));
+
+            Add(new ChemicalElement(17, 3, 17,
+                35.453f, "Chlorine", "Cl", 1251.2f,
+                3.16f, MetalClassificationTypes.Halogen));
+
+            Add(new ChemicalElement(18, 3, 18,
+                39.948f, "Argon", "Ar", 1520.6f,
+                0.0f, MetalClassificationTypes.NobleGas));
+
+            Add(new ChemicalElement(1, 4, 19,
+                39.0983f, "Potassium", "K", 418.8f,
+                0.82f, MetalClassificationTypes.AlkaliMetal));
+
+            Add(new ChemicalElement(2, 4, 20,
+                40.078f, "Calcium", "Ca", 589.8f,
+                1.00f, MetalClassificationTypes.AlkalineMetal));
+
+            Add(new ChemicalElement(3, 4, 21,
+                44.95591f, "Scandium", "Sc", 633.1f,
+                1.36f, MetalClassificationTypes.Lanthanoid));
+
+            Add(new ChemicalElement(4, 4, 22,
+                47.867f, "Titanium", "Ti", 658.8f,
+                1.54f, MetalClassificationTypes.Lanthanoid));
         }
     }
 }
--- Brysageek.Elements/Chemicals.cs
+++ Brysageek.Elements/Chemicals.cs
@@ -8,8 +8,27 @@ namespace Brysageek.Elements
 
         public static Chemicals All => ChemicalElements.Value;
         public static ChemicalElement Hydrogen => ChemicalElements.Value[0];
-        public static ChemicalElement Lithium => ChemicalElements.Value[1];
-        public static ChemicalElement Beryllium => ChemicalElements.Value[2];
+        public static ChemicalElement Helium => ChemicalElements.Value[1];
+        public static ChemicalElement Lithium => ChemicalElements.Value[2];
+        public static ChemicalElement Beryllium => ChemicalElements.Value[3];
+        public static ChemicalElement Boron => ChemicalElements.Value[4];
+        public static ChemicalElement Carbon => ChemicalElements.Value[5];
+        public static ChemicalElement Nitrogen => ChemicalElements.Value[6];
+        public static ChemicalElement Oxygen => ChemicalElements.Value[7];
+        public static ChemicalElement Fluorine => ChemicalElements.Value[8];
+        public static ChemicalElement Neon => ChemicalElements.Value[9];
+        public static ChemicalElement Sodium => ChemicalElements.Value[10];
+        public static ChemicalElement Magnesium => ChemicalElements.Value[11];
+        public static ChemicalElement Aluminum => ChemicalElements.Value[12];
+        public static ChemicalElement Silicon => ChemicalElements.Value[13];
+        public static ChemicalElement Phosphorus => ChemicalElements.Value[14];
+        public static ChemicalElement Sulfur => ChemicalElements.Value[15];
+        public static ChemicalElement Chlorine => ChemicalElements.Value[16];
+        public static ChemicalElement Argon => ChemicalElements.Value[17];
+        public static ChemicalElement Potassium => ChemicalElements.Value[18];
+        public static ChemicalElement Calcium => ChemicalElements.Value[19];
+        public static ChemicalElement Scandium => ChemicalElements.Value[20];
+        public static ChemicalElement Titanium => ChemicalElements.Value[21];
 
         private Chemicals(IList<ChemicalElement> list) : base(list)
         {
